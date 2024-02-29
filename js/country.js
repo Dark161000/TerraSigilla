a34f5a2ed0e0620eb259ccd62a68cc580c16e9c2
Zermeno Garcia <jose.zermenogarcia@here.com>
2024-02-28 23:37:37 -0600

    Initial commit

---
+++ js/country.js
@@ -0,0 +1,14 @@
+const countries = {
+    Austria: {
+        zoom: 7,
+        cities: {
+            Salzburg: {zoom: 12,},
+        },
+    },
+    Italy: {
+        zoom: 6,
+        cities: {
+            Rome: {zoom: 11,},
+        },
+    },
+};
