7029ac5ff864fdfb35494df86997cdbee50db487
Zermeno Garcia <jose.zermenogarcia@here.com>
2024-03-03 16:20:31 -0600

    url added to object

--- js/country.js
+++ js/country.js
@@ -2,7 +2,10 @@ const countries = {
     Austria: {
         zoom: 7,
         cities: {
-            Vienna: {zoom: 11,},
+            Vienna: {
+                zoom: 11,
+                url: 'https://m.wien.gv.at/stadtplan/#base=karte&overlay=adressen&zoom=11&layer=baustelle%2Ceinbahn&lon=16.4235&lat=48.1942',
+            },
         },
     },
     Italy: {
