a68519f8c11e259d03ada50ae5320d18dd23cec8
Zermeno Garcia <leonardo.zermenogarcia@here.com>
2024-03-08 22:47:45 -0600

    Updated urls and names

--- js/country.js
+++ js/country.js
@@ -4,7 +4,10 @@ const countries = {
         cities: {
             Vienna: {
                 zoom: 11,
-                url: 'https://m.wien.gv.at/stadtplan/#base=karte&overlay=adressen&zoom=11&layer=baustelle%2Ceinbahn&lon=16.4235&lat=48.1942',
+                source: {
+                    name: 'Stadt Wien',
+                    url: 'https://m.wien.gv.at/stadtplan/#base=karte&overlay=adressen&zoom=11&layer=baustelle%2Ceinbahn&lon=16.4235&lat=48.1942',
+                },
             },
         },
     },
@@ -13,7 +16,10 @@ const countries = {
         cities: {
             Berlin: {
                 zoom: 10,
-                url: 'https://viz.berlin.de/verkehr-in-berlin/baustellen-sperrungen-und-sonstige-storungen/?it=verkehr-in-berlin/baustellen-und-sperrungen/',
+                source: {
+                    name: 'Berlin.de',
+                    url: 'https://viz.berlin.de/verkehr-in-berlin/baustellen-sperrungen-und-sonstige-storungen/?it=verkehr-in-berlin/baustellen-und-sperrungen/',
+                },
             },
         },
     },
@@ -22,31 +28,52 @@ const countries = {
         cities: {
             CityofSanMarino: {
                 zoom: 12,
-                url: 'https://www.cciss.it/web/cciss/situazione-della-viabilita',
+                source: {
+                    name: 'CCISS',
+                    url: 'https://www.cciss.it/web/cciss/situazione-della-viabilita',
+                },
             },
             Florence: {
                 zoom: 10,
-                url: 'https://www.cciss.it/web/cciss/situazione-della-viabilita',
+                source: {
+                    name: 'CCISS',
+                    url: 'https://www.cciss.it/web/cciss/situazione-della-viabilita',
+                },
             },
             Milan: {
                 zoom: 12,
-                url: 'https://www.cciss.it/web/cciss/situazione-della-viabilita',
+                source: {
+                    name: 'CCISS',
+                    url: 'https://www.cciss.it/web/cciss/situazione-della-viabilita',
+                },
             },
             Naples: {
                 zoom: 10,
-                url: 'https://www.cciss.it/web/cciss/situazione-della-viabilita',
+                source: {
+                    name: 'CCISS',
+                    url: 'https://www.cciss.it/web/cciss/situazione-della-viabilita',
+                },
             },
             Palermo: {
                 zoom: 12,
-                url: 'https://www.cciss.it/web/cciss/situazione-della-viabilita',
+                source: {
+                    name: 'CCISS',
+                    url: 'https://www.cciss.it/web/cciss/situazione-della-viabilita',
+                },
             },
             Rome: {
                 zoom: 11,
-                url: 'https://www.cciss.it/web/cciss/situazione-della-viabilita',
+                source: {
+                    name: 'CCISS',
+                    url: 'https://www.cciss.it/web/cciss/situazione-della-viabilita',
+                },
             },
             Turin: {
                 zoom: 12,
-                url: 'https://www.cciss.it/web/cciss/situazione-della-viabilita',
+                source: {
+                    name: 'CCISS',
+                    url: 'https://www.cciss.it/web/cciss/situazione-della-viabilita',
+                },
             },
         },
     },
